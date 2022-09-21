adf06709f22ba13b81aa5dd2a642457fc8a33171
thiago sousa <thiagodesamorim@gmail.com>
2022-09-21 20:44:09 -0300

    Utilizando operadores lógicos para saber se a condição é verdadeira ou falsa

--- 01-fundamentos/bloco-04-introducao-a-java-script/ex.4.1.5.js
+++ 01-fundamentos/bloco-04-introducao-a-java-script/ex.4.1.5.js
@@ -5,3 +5,6 @@ if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "qu
 else { 
     console.log("FINALMENTE, descanso merecido UwU")
 }
+
+// utilizando operadores lógicos com estruturas condicionais para saber se a condição é false ou true.
+// o resultado correto foi "Oba, mais um dia de aprendizado na Trybe" pois utilizamos o operador || (ou) para saber se o weekDay era um dos dias da lista, e como o dia é quarta, deu True.
