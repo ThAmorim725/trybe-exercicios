ac33e26c878f0bc398c75f0ba5f48c8d9fda436c
thiago sousa <thiagodesamorim@gmail.com>
2022-09-22 15:45:22 -0300

    utilizando o array indice para pegar um elemento dentro da lista

---
+++ 01-fundamentos/bloco-04-introducao-a-java-script/dia-02-array-loop-for-js/ex.4.2.1.js
@@ -0,0 +1,7 @@
+let menu = ["home", "serviços", "portfólio", "links"];
+let menuServiços = menu[1];
+
+console.log(menuServiços);
+
+// utilizando o array para pegar um indice dentro da lista
+// como eu tinha que pegar a string serviços, chamei a váriavel menu + o indici [1] pois na programação a contagem começa do zero.
