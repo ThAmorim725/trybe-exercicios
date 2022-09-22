741a2e3324841b0c5963436651fc3c02c6d70e56
thiago sousa <thiagodesamorim@gmail.com>
2022-09-22 15:52:55 -0300

    Procurando um indice dentro do array através do .indexOF()

---
+++ 01-fundamentos/bloco-04-introducao-a-java-script/dia-02-array-loop-for-js/ex.4.2.2.js
@@ -0,0 +1,6 @@
+let menu = ["Home", "Serviços", "Portfólio", "Links"];
+let indexOfPortfolio = menu.indexOf("Portfólio")
+
+console.log(indexOfPortfolio);
+
+// utilizando o .indexOf() para saber em qual indice o elemento está
