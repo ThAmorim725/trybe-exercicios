93c746e620785f5cf47c6dd9f6cbb6f2cb2e4978
Thiago Sousa <112829868+ThAmorim725@users.noreply.github.com>
2022-09-21 21:45:19 -0300

    Delete ex.4.1.2.js

--- 01-fundamentos/bloco-04-introducao-a-java-script/ex.4.1.2.js
+++
@@ -1,6 +0,0 @@
-const base = 5; 
-const heigth = 8;
-const area = 40;
-const perimeter = 53;
-console.log (base * heigth)  // imprimir para saber a multiplicação de 5x8
-console.log (base + heigth + area) // imprimir para saber a soma de 5 + 8 + 40
